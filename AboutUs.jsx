bd79ae9e9b93ccabac6a64a23e1cfaf843d69752
DK-1717 <23dit020@charusat.edu.in>
2026-02-19 21:27:59 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is your one-stop destination for beautiful
+        and healthy houseplants.
+      </p>
+      <p>
+        Our mission is to bring nature closer to your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
